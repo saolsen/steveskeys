8ea51ae308dec3ffba3c3b79ec0ba0ca93cde7a3
Stephen Olsen <steve.olsen@gmail.com>
2012-11-13 23:33:34 -0500

    docs

--- README.md
+++ README.md
@@ -5,9 +5,44 @@ http://precog.com/blog-precog-2/entry/do-you-have-what-it-takes-to-be-a-precog-e
 Key value storage implementation based on the precog challenge problem
 but in clojure instead of scala.
 
-## Usage
+## Requirments
 
-The storage can be used by creating an instance of StevesKeys.
+This is a diskstore modeled after the requirments of the precog
+challenge problem. The problem is to create a disk backed key value
+store that implements some scala traits. This disk backed key value
+store is instead written in clojure and implements a similar interface
+but in a clojure idiomatic way.
+
+The functional constraints of this implementation match the scala
+requirments.
+
+* The write performance must approach the linear write performance of the
+  hard disk as measured by raw Java IO.
+* Duplicate values cannot be stored more than once. That is, if you add 10000
+  keys all with the value "John Doe", then the text "John Doe" must be stored
+  only a single time.
+* flush! must obey the obvious constraint.
+* If the program is forcibly terminated at any point during writing to a disk
+  store, then retrieving the disk store may not fail and must preserve all
+  all information prior to the most recent call to flush().
+* Assume the number of unique values (and the number of keys) is too
+  great to fit in memory.
+
+Ways this differs from a scala requirments.
+
+* instead of traverse returning a Reader it will return a lazy
+  sequence.
+* get is used in clojure and I didn't want to override it here so even
+  though it doesn't mutate the DiskStore I still named the get method
+  get!. I'm open to better suggestions.
+
+## Implementation Details
+
+* I'm using nippy for serialization of clojure data structures to byte
+arrays so anything serializable by nippy is supported as a key or
+value.
+* I'm using google guava's byte-array comparator because java doesn't
+  have one built in.
 
 ## License
 
--- src/saolsen/steveskeys.clj
+++ src/saolsen/steveskeys.clj
@@ -3,41 +3,6 @@
   (:import java.util.Arrays
            com.google.common.primitives.UnsignedBytes))
 
-;; This is a diskstore modeled after the requirments of the precog challenge
-;; problem. The problem is to create a disk backed key value store that
-;; implements some scala traits. This disk backed key value store is instead
-;; written in clojure and implements a similar intervace but in a clojure
-;; idiomatic way.
-
-;; The functional constraints of this implementation match those of the scala
-;; requirments.
-
-;; * The write performance must approach the linear write proformance of the
-;;   hard disk as measured by raw Java IO.
-;; * Duplicate values cannot be stored more than once. That is, if you add 10000
-;;   keys all with the value "John Doe", then the text "John Doe" must be stored
-;;   only a single time.
-;; * flush! must obey the obvious constraint.
-;; * If the program is forcibly terminated at any point during writing to a disk
-;;   store, then retrieving the disk store may not fail and must preserve all
-;;   all information prior to the most recent call to flush().
-;; * Assume the number of unique values (and the number of keys) is too great to
-;;  fit in memory.
-
-;; Ways this differs from a scala implementation is that instead of traverse
-;; returning a Reader it will return a lazy sequence. This is not meant to be
-;; an exact implementation of the precog challenge (as I am not interviewing
-;; with them) it is just meant to be a similar artifact that's useable from
-;; clojure. (and to be a great systems programming challenge)
-
-;; Get is used in clojure and I didn't want to override it here so even though
-;; it doesn't mutate the DiskStore I still named the get method get!. I'm open
-;; to better suggestions.
-
-;; I'm using nippy for serialization of clojure data structures but I'm not
-;; using it for the ordering for traverse. The ordering is going to be standard
-;; clojure data structure ordering.
-
 (defn bequals
   "Equality operator for byte arrays"
   [a b]
