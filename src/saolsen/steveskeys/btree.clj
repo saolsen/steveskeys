8c7dcb1d9b63f70a9fef5c7ac3efaa619571e8b9
Stephen Olsen <steve.olsen@gmail.com>
2012-11-16 17:18:19 -0500

    btree supports same associable api as maps, so you can use get and assoc

--- src/saolsen/steveskeys/btree.clj
+++ src/saolsen/steveskeys/btree.clj
@@ -20,10 +20,13 @@
   (.compare byte-array-comparator a b))
 
 ;; Persistant B+ tree implementation.
+;; An immutable and persistant B+ tree implementation that can be used anywhere
+;; immutable structures are required (eg, atoms)
+;;
 ;; Assumptions:
-;; - keys and values are byte arrays, this is specific for the parameters of
-;;   this problem but if this tree implementation is to be used in any other
-;;   setting it will need to be genericised.
+;; - keys and values are byte arrays.
+;; - get-node is a referentially transparent function.
+;; - (= node (get-node (add-node node)))
 
 ;; Each node has a list of key value pairs.
 ;; {:kvps [[key1 node-id1] [key2 node-id2]] }
@@ -31,44 +34,39 @@
 ;; {:kvps [[key1 val1] [key2 val2]] }
 (defrecord BPlusTreeLeaf [kvps])
 
-(defprotocol IPersistantBPlusTree
-  "A persistant B+ tree"
-  (getnode [this id]
-    "gets a tree node by id")
-  (addnode [this value]
-    "adds a new node, returns the new node id")
-  (search [this key] "searches the tree for the value associated with the key"))
-
 (defn- search-step-reducer
   "reduce function over the key value pairs"
   [search-key result kvp]
   (or result
       (let [[key value] kvp
             c (bcompare search-key key)]
+        ;; if the key is in the correct range for this node return the value
         (if (<= c 0)
           value
           nil))))
 
 (defn- search-step
-  "returns the value or the next node id, nil value means it's not found"
+  "returns the value or the next node id, {:value nil} means it's not found"
   [key node]
   (if (instance? BPlusTreeNode node)
+    ;; node
     (let [kvps (:kvps node)
           next (reduce (partial search-step-reducer key) nil (butlast kvps))]
       {:next (or next (second (last kvps)))})
     ;; leaf
     {:value (second (first (filter #(bequals key (first %)) (:kvps node))))}))
 
-;; Getting and setting of nodes, pulling them from the cache etc is not managed
-;; here. The tree takes a getter and a setter for retreiving the nodes, this way
-;; it can be used independant of how the nodes are stored.
-(defrecord PersistantBPlusTree [root get-node add-node]
-  IPersistantBPlusTree
-  (getnode [_ id] (get-node id))
-  (addnode [_ value] (add-node value))
-  (search [this key]
+(deftype PersistantBPlusTree [root get-node add-node]
+  clojure.lang.Associative
+  ;; assoc
+  (assoc [_ key value] {:it 1}) ;woohoo, this has to return an associative!
+
+  ;; get
+  (valAt [_ key]
+    ;; Recursively search down the tree for the key, returns it's value.
     (loop [node root]
       (let [step (search-step key node)
             next (:next step)]
         (or (:value step)
-            (when next (recur (getnode this next))))))))
+            (when next (recur (get-node next)))))))
+)
--- test/saolsen/steveskeys/btree_test.clj
+++ test/saolsen/steveskeys/btree_test.clj
@@ -5,37 +5,37 @@
 
 (def nodes
   {
-   :root (bplus-tree-node [[(nippy/freeze-to-bytes 2) :a]
+   :root (->BPlusTreeNode [[(nippy/freeze-to-bytes 2) :a]
                            [(nippy/freeze-to-bytes 5) :b]
                            [(nippy/freeze-to-bytes 9) :c]
                            [nil :d]])
-   :a (bplus-tree-leaf [[(nippy/freeze-to-bytes 1) "one"]
+   :a (->BPlusTreeLeaf [[(nippy/freeze-to-bytes 1) "one"]
                         [(nippy/freeze-to-bytes 2) "two"]])
-   :b (bplus-tree-leaf [[(nippy/freeze-to-bytes 3) "three"]
+   :b (->BPlusTreeLeaf [[(nippy/freeze-to-bytes 3) "three"]
                         [(nippy/freeze-to-bytes 4) "four"]
                         [(nippy/freeze-to-bytes 5) "five"]])
-   :c (bplus-tree-leaf [[(nippy/freeze-to-bytes 6) "six"]
+   :c (->BPlusTreeLeaf [[(nippy/freeze-to-bytes 6) "six"]
                         [(nippy/freeze-to-bytes 7) "seven"]
                         [(nippy/freeze-to-bytes 8) "eight"]
                         [(nippy/freeze-to-bytes 9) "nine"]])
-   :d (bplus-tree-leaf [[(nippy/freeze-to-bytes 10) "ten"]])
+   :d (->BPlusTreeLeaf [[(nippy/freeze-to-bytes 10) "ten"]])
    }
   )
 
 (deftest test-btree-search
   (testing "Search for a key")
   (let [tree (->PersistantBPlusTree (:root nodes) #(get nodes %) #(%))]
-    (is (= (search tree (nippy/freeze-to-bytes 1)) "one"))
-    (is (= (search tree (nippy/freeze-to-bytes 2)) "two"))
-    (is (= (search tree (nippy/freeze-to-bytes 3)) "three"))
-    (is (= (search tree (nippy/freeze-to-bytes 4)) "four"))
-    (is (= (search tree (nippy/freeze-to-bytes 5)) "five"))
-    (is (= (search tree (nippy/freeze-to-bytes 6)) "six"))
-    (is (= (search tree (nippy/freeze-to-bytes 7)) "seven"))
-    (is (= (search tree (nippy/freeze-to-bytes 8)) "eight"))
-    (is (= (search tree (nippy/freeze-to-bytes 9)) "nine"))
-    (is (= (search tree (nippy/freeze-to-bytes 10)) "ten"))
-    (is (= (search tree (nippy/freeze-to-bytes 11)) nil))
+    (is (= (get tree (nippy/freeze-to-bytes 1)) "one"))
+    (is (= (get tree (nippy/freeze-to-bytes 2)) "two"))
+    (is (= (get tree (nippy/freeze-to-bytes 3)) "three"))
+    (is (= (get tree (nippy/freeze-to-bytes 4)) "four"))
+    (is (= (get tree (nippy/freeze-to-bytes 5)) "five"))
+    (is (= (get tree (nippy/freeze-to-bytes 6)) "six"))
+    (is (= (get tree (nippy/freeze-to-bytes 7)) "seven"))
+    (is (= (get tree (nippy/freeze-to-bytes 8)) "eight"))
+    (is (= (get tree (nippy/freeze-to-bytes 9)) "nine"))
+    (is (= (get tree (nippy/freeze-to-bytes 10)) "ten"))
+    (is (= (get tree (nippy/freeze-to-bytes 11)) nil))
 ))
 
 ;(deftest test-btree-search
