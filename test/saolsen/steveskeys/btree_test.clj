4780af20e4c8eaa8a58997074b75bb5d1fab36f7
Stephen Olsen <steve.olsen@gmail.com>
2012-11-20 11:20:13 -0500

    expanded test cases, am getting some errors. My guess is splitting internal nodes wasn't happening before and is now and thats where I'll first look for the bug

--- test/saolsen/steveskeys/btree_test.clj
+++ test/saolsen/steveskeys/btree_test.clj
@@ -3,9 +3,6 @@
   (:use clojure.test
         saolsen.steveskeys.btree))
 
-(def ks [1 2 3 4 5 6 7 8 9 10])
-(def vs ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"])
-
 ;; Manually built tree for testing the search
 (def test-nodes
   {
@@ -36,25 +33,37 @@
    }
   )
 
+(def ks [1 2 3 4 5 6 7 8 9 10])
+(def vs ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"])
+
+(def ks2 (range 100))
+(def chars (map char (range 97 (+ 97 26))))
+(def vs2 (take 100
+               (flatten
+                (map
+                 (fn [a]
+                   (map (fn [b] (str a b)) chars))
+                 chars))))
+
 (defn test-they-all-exist
   "checks that all ks are mapped to vs in tree, also checks that if the key
    isn't in the tree it returns nil"
-  [tree]
+  [tree ks vs]
   (doseq [[k v] (map #(vector %1 %2) ks vs)]
     (let [result (get tree (nippy/freeze-to-bytes k))]
       (is (= result v))))
   (is (= (get tree (nippy/freeze-to-bytes 11)) nil)))
 
-;; (deftest test-btree-search
-;;   (testing "Search for a key in a premade tree")
-;;   (let [tree
-;;         (->PersistantBPlusTree (:root test-nodes) #(get test-nodes %) nil 1)]
-;;    (test-they-all-exist tree)))
+(deftest test-btree-search
+   (testing "Search for a key in a premade tree")
+   (let [tree
+         (->PersistantBPlusTree (:root test-nodes) #(get test-nodes %) nil 1)]
+    (test-they-all-exist tree ks vs)))
 
 ;; Tree that stores the nodes in a clojure map, used to test construction.
 (deftest test-btree-buildin
   (testing "Build and search a tree, inserting in a bunch of random orders")
-  (dotimes [n 50]
+  (dotimes [n 10]
     (let [nextid (ref 0)
           nodes (ref {:root (->BPlusTreeLeaf [])})
           tree (->PersistantBPlusTree (:root @nodes)
@@ -65,8 +74,12 @@
                                           id))
                                       4)
           s (flatten
-             (shuffle (map #(vector (nippy/freeze-to-bytes %1) %2) ks vs)))
+             (shuffle (map #(vector (nippy/freeze-to-bytes %1) %2) ks2 vs2)))
           _ (println s)
           added
-          (apply (partial assoc tree) s)]
-      (test-they-all-exist added))))
+          (apply (partial assoc tree) s)
+          _ (test-they-all-exist added ks2 vs2)
+          ;; test that after tree is made, duplicates can be replaced correctly
+          t (apply (partial assoc added) (map #(vector)))]
+
+)))
